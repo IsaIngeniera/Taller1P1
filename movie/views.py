efeda3e5795d03fbb96aaf847ef806858385c054
Isa <isabellaocampo128@gmail.com>
2026-02-06 16:51:11 -0500

    cambio de mi nombre en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Hola mundo, lo logré</h1>')
     #return render(request, 'home.html')
-    return render(request , 'home.html', {'name': 'Isabella'})
+    return render(request , 'home.html', {'name': 'Isabella Ocampo S'})
     
 def about(request):
     return HttpResponse('<h1>Hola, estoy en La subpagina about</h1>')
